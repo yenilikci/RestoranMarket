9495512fdfa781d4464a7fc879921f4968f92b72
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-01-01 23:25:10 +0300

    Account Controller Ve View'i Eklendi, Anasayfa Düzenlendi

---
+++ RestoranMarket/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestoranMarket.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+    }
+}
