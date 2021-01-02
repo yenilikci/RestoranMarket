b473b188647a8a2d94e5aab20e51bfe1bda1d641
Klewolas <48567287+Klewolas@users.noreply.github.com>
2021-01-02 15:58:03 +0300

    Rol Yönetimi Eklendi.

--- RestoranMarket/Controllers/AdminRoleController.cs
+++ RestoranMarket/Controllers/AdminRoleController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using RestoranMarket.Identity;
+using RestoranMarket.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,12 @@ namespace RestoranMarket.Controllers
     public class AdminRoleController : Controller
     {
         private RoleManager<IdentityRole> roleManager;
+        private UserManager<ApplicationUser> userManager;
 
-        public AdminRoleController(RoleManager<IdentityRole> _roleManager)
+        public AdminRoleController(RoleManager<IdentityRole> _roleManager, UserManager<ApplicationUser> _userManager)
         {
             roleManager = _roleManager;
+            userManager = _userManager;
         }
 
         public IActionResult Index()
@@ -48,7 +52,87 @@ namespace RestoranMarket.Controllers
             return View(name);    
         }
 
-        public async Task<IActionResult> Delete(string id)
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+
+            //rol içerisindeki kayıtlı kullanıcılarım
+            var members = new List<ApplicationUser>();
+            //rol içerisine atanabilecek kullanıcılarım
+            var nonmembers = new List<ApplicationUser>();
+
+            foreach (var user in userManager.Users)
+            {
+                //burada gelen kullanıcın rolünü kontrol ediyoruz
+                var list = await userManager.IsInRoleAsync(user, role.Name)
+                    ? members : nonmembers;
+                list.Add(user);
+            }
+
+            var model = new RoleDetails()
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonmembers
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RoleEditModel model)
+        {
+            IdentityResult result;
+
+            if (ModelState.IsValid)
+            {
+                foreach (var userId in model.IdsToAdd ?? new string[] { })
+                {
+                    var user = await userManager.FindByIdAsync(userId);
+
+                    if (user != null)
+                    {
+                        result = await userManager.AddToRoleAsync(user, model.RoleName);
+
+                        if (!result.Succeeded)
+                        {
+                            foreach (var error in result.Errors)
+                            {
+                                ModelState.AddModelError("", error.Description);
+                            }
+                        }
+                    }
+                }
+
+                foreach (var userId in model.IdsToDelete ?? new string[] { })
+                {
+                    var user = await userManager.FindByIdAsync(userId);
+
+                    if (user != null)
+                    {
+                        result = await userManager.RemoveFromRoleAsync(user, model.RoleName);
+
+                        if (!result.Succeeded)
+                        {
+                            foreach (var error in result.Errors)
+                            {
+                                ModelState.AddModelError("", error.Description);
+                            }
+                        }
+                    }         
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Edit", model.RoleId);
+            }
+        }
+
+            public async Task<IActionResult> Delete(string id)
         {
             var role = await roleManager.FindByIdAsync(id);
 
---
+++ RestoranMarket/Models/RoleModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RestoranMarket.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestoranMarket.Models
+{
+    public class RoleDetails
+    {
+        public IdentityRole Role { get; set; }
+        //Rol içerisindeki kullanıcılar;
+        public IEnumerable<ApplicationUser> Members { get; set; }
+        //Rol içerisinde olmayan kullanıcılar;
+        public IEnumerable<ApplicationUser> NonMembers { get; set; }
+    }
+
+    public class RoleEditModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string[] IdsToAdd{ get; set; }
+        public string[] IdsToDelete { get; set; }
+
+    }
+}
